749660ba6466b229ef1962df3e688cf170c1e8aa
Thomas Bohmbach, Jr <thomas@gumption.com>
2013-03-01 12:28:30 -0600

    Remove drone.io path from test script

--- test_main.py
+++ test_main.py
@@ -1,8 +1,7 @@
 import os
 import sys
-dev_appserver = None
-sdk_path = ""
-for d in os.environ["PATH"].split(":") + ['./google_appengine']:
+
+for d in os.environ["PATH"].split(":"):
     dev_appserver_path = os.path.join(d, "dev_appserver.py")
     if os.path.isfile(dev_appserver_path):
         sdk_path = os.path.abspath(os.path.dirname(os.path.realpath(dev_appserver_path)))
