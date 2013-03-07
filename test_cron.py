77da66c89597c9b1d047671c7cdae845aef75900
Thomas Bohmbach, Jr <thomas@gumption.com>
2013-03-07 09:22:51 -0600

    Trying to fix odd drone.io test issue

--- test_cron.py
+++ test_cron.py
@@ -1,6 +1,16 @@
+import os
+import sys
+
+for d in os.environ["PATH"].split(":"):
+    dev_appserver_path = os.path.join(d, "dev_appserver.py")
+    if os.path.isfile(dev_appserver_path):
+        sdk_path = os.path.abspath(os.path.dirname(os.path.realpath(dev_appserver_path)))
+        sys.path.insert(0, sdk_path)
+        import dev_appserver
+        dev_appserver.fix_sys_path()
+
 import datetime
 import hashlib
-import os
 
 from google.appengine.api.users import User
 
