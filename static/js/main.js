55199880d64ff1b9fd863bc1074a9fef50735e8d
vitaflo <studio@brentgustafson.com>
2013-03-07 21:37:24 -0600

    properly resize background

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,20 @@
 *  (C) 2012 - Brent Gustafson - brentgustafson.com
 ******************************************************/
 
-$(function() {
+var bg, bgh, bgw;
 
+function resize_bg() {
+    bgw = bg.width();
+    offsetw = bgw - $(window).width();
+    bg.css("left", offsetw/2*-1);
+}
+
+$(function() {
+    bg = $("#bg");
+    resize_bg();
+    
+    $(window).resize(function () {
+        resize_bg();
+    });
+    
 });
