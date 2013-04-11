6234b607a4ca71790f2352edb70437f1b9fc8c19
Mark Larimer <mark.d.larimer@gmail.com>
2013-04-11 00:27:32 -0500

    Added "Users" and "Screens" menu items. Moved and tweaked chat form. Other miscellaneous format tweaks, including screenshots pages.

--- static/js/chats.js
+++ static/js/chats.js
@@ -126,26 +126,23 @@ var scroller = {
 };
 
 function enableFormSubmission(form) {
-    form.find('input[type="submit"]').on('click', function(e) {
+    form.on('submit', function(e) {
         e.preventDefault();
         submitForm(form);
     });
 }
 
 function submitForm(form) {
-    form.find('input[type="submit"]').attr('disabled', 'disabled');
     var url = form.attr('action');
     $.ajax({
         type: 'POST',
         url: url,
         data: form.serialize(),
         success: function(result) {
-            form.find('input[type="submit"]').removeAttr('disabled');
             form.find('input[type="text"]').val('');
         }
     });
 }
-
 
 $(function() {
     chats.init();
