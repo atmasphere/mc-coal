147ffb75cadad96fea07ad42ac0a46b2e4119d21
Thomas Bohmbach, Jr <thomas@gumption.com>
2013-09-23 15:25:35 -0500

    Consolidate test setup code in agar.test.base_test

--- test_channel.py
+++ test_channel.py
@@ -1,19 +1,11 @@
-import os
-import sys
-import minimock
 import json
 
-for d in os.environ["PATH"].split(":"):
-    dev_appserver_path = os.path.join(d, "dev_appserver.py")
-    if os.path.isfile(dev_appserver_path):
-        sdk_path = os.path.abspath(os.path.dirname(os.path.realpath(dev_appserver_path)))
-        sys.path.insert(0, sdk_path)
-        import dev_appserver
-        dev_appserver.fix_sys_path()
-
 from google.appengine.api import channel
+
 from agar.test.base_test import BaseTest
 from agar.test.web_test import WebTest
+
+import minimock
 
 import channel as coal_channel
 import models
--- test_models.py
+++ test_models.py
@@ -1,18 +1,10 @@
 import base64
 import datetime
 import os
-import sys
-import minimock
-
-for d in os.environ["PATH"].split(":"):
-    dev_appserver_path = os.path.join(d, "dev_appserver.py")
-    if os.path.isfile(dev_appserver_path):
-        sdk_path = os.path.abspath(os.path.dirname(os.path.realpath(dev_appserver_path)))
-        sys.path.insert(0, sdk_path)
-        import dev_appserver
-        dev_appserver.fix_sys_path()
 
 from google.appengine.ext import blobstore, testbed, deferred, ndb
+
+import minimock
 
 from agar.test.base_test import BaseTest
 
