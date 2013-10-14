9d91c725c0527a50ebfe15f179a0e2ab1d5df4a2
Thomas Bohmbach, Jr <thomas@gumption.com>
2013-10-13 23:07:30 -0500

    Improve stop script end detection

--- mc-stop.sh
+++ mc-stop.sh
@@ -4,7 +4,7 @@ if [ "$PID" != "" ]; then
   echo "Stopping MineCraft Server PID=$PID"
   echo save-all >> command-fifo
   echo stop >> command-fifo
-  wait $PID
+  while kill -0 "$PID" > /dev/null 2>&1; do sleep 0.5; done
   rm server.pid
   echo "MineCraft shutdown complete."
 else
