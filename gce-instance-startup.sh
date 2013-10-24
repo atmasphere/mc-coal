ce01e69e70b0128a561931253e087f8e7dc7b57a
Thomas Bohmbach, Jr <thomas@gumption.com>
2013-10-24 13:10:57 -0500

    #30 Fix instance status message

--- gce-instance-startup.sh
+++ gce-instance-startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+
 apt-get -y install openjdk-7-jre
-# CONTROLLER_URL=$(curl http://metadata/computeMetadata/v1beta1/instance/attributes/controller-url)
 mkdir minecraft
 cd minecraft
 wget https://s3.amazonaws.com/Minecraft.Download/versions/1.6.4/minecraft_server.1.6.4.jar -O minecraft_server.jar
-# wget $CONTROLLER_URL
