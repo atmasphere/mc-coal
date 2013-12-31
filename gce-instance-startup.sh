c73febe3ec4bfc354769a14785154d8e912f365f
Thomas Bohmbach, Jr <thomas@gumption.com>
2013-12-31 13:04:11 -0600

    #30 Fix instance startup script

--- gce-instance-startup.sh
+++ gce-instance-startup.sh
@@ -20,7 +20,7 @@ mkdir coal
 cd coal
 
 PROJECT_ID=$(curl http://metadata/computeMetadata/v1beta1/instance/attributes/project_id)
-$PROJECT_MC_URL="https://$PROJECT_ID.appspot.com/mc"
+PROJECT_MC_URL="https://$PROJECT_ID.appspot.com/mc"
 curl http://metadata/computeMetadata/v1beta1/instance/attributes/project-id -o project_id
 
 curl http://metadata/computeMetadata/v1beta1/instance/attributes/timezones -o timezones.py
@@ -28,10 +28,10 @@ curl http://metadata/computeMetadata/v1beta1/instance/attributes/timezones -o ti
 curl http://metadata/computeMetadata/v1beta1/instance/attributes/agent-script -o mc_coal_agent.py
 chmod a+x mc_coal_agent.py
 
-$MC_PROP_URL="$PROJECT_MC_URL/server.properties"
+MC_PROP_URL="$PROJECT_MC_URL/server.properties"
 wget $MC_PROP_URL -O server.properties
 
-$CONTROLLER_URL="$PROJECT_MC_URL/mc_coal_controller.py"
+CONTROLLER_URL="$PROJECT_MC_URL/mc_coal_controller.py"
 wget $CONTROLLER_URL -O mc_coal_controller.py
 chmod a+x mc_coal_controller.py
 ./mc_coal_controller.py &
