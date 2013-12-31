b1b844a3478313ad78b2379a55bff5a4113e5411
Thomas Bohmbach, Jr <thomas@gumption.com>
2013-12-31 13:16:29 -0600

    #30 Add printf to instance start script to debug

--- gce-instance-startup.sh
+++ gce-instance-startup.sh
@@ -6,6 +6,7 @@ pip install --upgrade google-api-python-client
 pip install pyyaml
 pip install requests
 pip install psutil
+pip install git
 pip install git+https://github.com/twoolie/NBT@version-1.4.1#egg=NBT
 
 MINECRAFT_URL=$(curl http://metadata/computeMetadata/v1beta1/instance/attributes/minecraft-url)
@@ -20,7 +21,9 @@ mkdir coal
 cd coal
 
 PROJECT_ID=$(curl http://metadata/computeMetadata/v1beta1/instance/attributes/project_id)
+printf "PROJECT_ID: $PROJECT_ID"
 PROJECT_MC_URL="https://$PROJECT_ID.appspot.com/mc"
+printf "PROJECT_MC_URL: $PROJECT_MC_URL"
 curl http://metadata/computeMetadata/v1beta1/instance/attributes/project-id -o project_id
 
 curl http://metadata/computeMetadata/v1beta1/instance/attributes/timezones -o timezones.py
@@ -29,9 +32,11 @@ curl http://metadata/computeMetadata/v1beta1/instance/attributes/agent-script -o
 chmod a+x mc_coal_agent.py
 
 MC_PROP_URL="$PROJECT_MC_URL/server.properties"
+printf "MC_PROP_URL: $MC_PROP_URL"
 wget $MC_PROP_URL -O server.properties
 
 CONTROLLER_URL="$PROJECT_MC_URL/mc_coal_controller.py"
+printf "CONTROLLER_URL: $CONTROLLER_URL"
 wget $CONTROLLER_URL -O mc_coal_controller.py
 chmod a+x mc_coal_controller.py
 ./mc_coal_controller.py &
