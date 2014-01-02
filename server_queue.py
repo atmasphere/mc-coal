6316f464d6fb7d9acc55f23f7c3ef67dc7879ffd
Thomas Bohmbach, Jr <thomas@gumption.com>
2014-01-02 13:22:16 -0600

    #30 Start minecraft and agent from controller

--- server_queue.py
+++ server_queue.py
@@ -9,6 +9,7 @@ def queue_controller_task(payload):
     payload = json.dumps(payload)
     taskqueue.Queue('controller').add([taskqueue.Task(payload=payload, method='PULL')])
 
+
 def start_server(server):
     if gce.is_setup():
         instance = gce.Instance.singleton()
@@ -16,7 +17,10 @@ def start_server(server):
             instance.start()
         payload = {'event': 'START_SERVER'}
         payload['server_key'] = server.key.urlsafe()
+        payload['agent_client_id'] = server.agent.client_id
+        payload['agent_secret'] = server.agent.secret
         queue_controller_task(payload)
+
 
 def stop_server(server):
     if gce.is_setup():
--- static/mc/mc_coal_controller.py
+++ static/mc/mc_coal_controller.py
@@ -6,6 +6,8 @@ import json
 import logging
 import os
 import shutil
+import signal
+import subprocess
 import sys
 import time
 
@@ -63,14 +65,28 @@ def get_servers():
     return servers
 
 
-def copy_server_files(port):
+def copy_server_properties(port, server_properties):
+    default_properties = os.path.join(COAL_DIR, 'server.properties')
+    server_dir = get_server_dir(port)
+    new_properties = os.path.join(server_dir, 'server.properties')
+    server_properties['server-port'] = port
+    with open(new_properties, "w") as fout:
+        with open(default_properties, "r") as fin:
+            for line in fin:
+                for prop in server_properties:
+                    if line.startswith(prop):
+                        line = "{0}={1}".format(prop, server_properties[prop])
+                fout.write(line)
+
+
+def copy_server_files(port, server_properties):
     server_dir = get_server_dir(port)
     shutil.copy2(os.path.join(COAL_DIR, 'minecraft_server.jar'), server_dir)
+    shutil.copy2(os.path.join(COAL_DIR, 'log4j2.xml'), server_dir)
+    copy_server_properties(port, server_properties)
     filenames = [
         'timezones.py',
-        'mc_coal_agent.py',
-        'mc_start.py',
-        'mc_stop.py'
+        'mc_coal_agent.py'
     ]
     mc_coal_dir = os.path.join(server_dir, 'mc_coal')
     if not os.path.exists(mc_coal_dir):
@@ -79,32 +95,86 @@ def copy_server_files(port):
         shutil.copy2(os.path.join(COAL_DIR, fn), mc_coal_dir)
 
 
+def make_fifo(server_dir):
+    fifo = os.path.join(server_dir, 'command-fifo')
+    try:
+        os.remove(fifo)
+    except OSError:
+        pass
+    os.mkfifo(fifo)
+    return fifo
+
+
 def start_server(server_key, **kwargs):
-    if server_key not in get_servers().keys():
+    servers = get_servers()
+    if server_key in servers.keys():
+        return
+    port = get_free_port()
+    server_dir = get_server_dir(port)
+    while os.path.exists(server_dir):
+        if read_server_key(port) == server_key:
+            break
         port = get_free_port()
         server_dir = get_server_dir(port)
-        while os.path.exists(server_dir):
-            if read_server_key(port) == server_key:
-                break
-            port = get_free_port()
-            server_dir = get_server_dir(port)
-        if not os.path.exists(server_dir):
-            os.makedirs(server_dir)
-            write_server_key(port, server_key)
-            copy_server_files(port)
-    #agent_start
-    #mc_start
+    if not os.path.exists(server_dir):
+        os.makedirs(server_dir)
+        write_server_key(port, server_key)
+        server_properties = kwargs.get('server_properties', {})
+        copy_server_files(port, server_properties)
+    try:
+        fifo = make_fifo(server_dir)
+        # Start Agent
+        mc_coal_dir = os.path.join(server_dir, 'mc_coal')
+        agent = os.path.join(mc_coal_dir, 'mc_coal_agent.py')
+        args = [agent]
+        args.append('--coal_host={0}.appspot.com'.format(project))
+        args.append('--agent_client_id={0}'.format(kwargs['agent_client_id']))
+        args.append('--agent_secret={0}'.format(kwargs['agent_secret']))
+        pid = subprocess.Popen(args).pid
+        pid_filename = os.path.join(server_dir, 'agent.pid')
+        with open(pid_filename, 'w') as pid_file:
+            pid_file.write(pid)
+        # Start MC
+        mc_jar = os.path.join(server_dir, 'minecraft_server.jar')
+        log4j = os.path.join(server_dir, 'log4j2.xml')
+        args = ['java', '-Xmx1G' '-Xms1G']
+        args.append('-Dlog4j.configurationFile={0}'.format(log4j))
+        args.append('-jar')
+        args.append(mc_jar)
+        args.append('nogui')
+        with open(fifo, 'rw') as fifo_file:
+            pid = subprocess.Popen(args, stdout=fifo_file, stdin=fifo_file).pid
+        pid_filename = os.path.join(server_dir, 'server.pid')
+        with open(pid_filename, 'w') as pid_file:
+            pid_file.write(pid)
+    except Exception, e:
+        logger.error(e)
 
 
 def stop_server(server_key, **kwargs):
     servers = get_servers()
     server = servers.get(server_key, None)
-    if server is not None:
-        port = server['port']
-        server_dir = get_server_dir(port)
-        #mc_stop
-        #agent_stop
-        shutil.rmtree(server_dir)
+    if server is None:
+        return
+    port = server['port']
+    server_dir = get_server_dir(port)
+    # Stop MC
+    fifo = os.path.join(server_dir, 'command-fifo')
+    with open(fifo, 'w') as fifo_file:
+        fifo_file.write('save-all')
+        fifo_file.write('stop')
+    pid = open(os.path.join(server_dir, 'server.pid'), 'r').read()
+    running = True
+    while running:
+        try:
+            os.kill(int(pid), 0)
+            time.sleep(1.0)
+        except OSError:
+            running = False
+    # Stop Agent
+    pid = open(os.path.join(server_dir, 'agent.pid'), 'r').read()
+    os.kill(int(pid), signal.SIGTERM)
+    shutil.rmtree(server_dir)
 
 
 def complete_tasks(tasks):
