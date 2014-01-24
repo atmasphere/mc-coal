1815c3d333708a1abd81de995f02246ad7eb4c0b
Thomas Bohmbach, Jr <thomas@gumption.com>
2014-01-23 22:57:39 -0600

    Update docs copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MC COAL'
-copyright = u'2013, mc-coal.org'
+copyright = u'2014, mc-coal.org'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
