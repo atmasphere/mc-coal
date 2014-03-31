9f9c66a7c68d6632b3aa35020499da67e8205a18
Brent Gustafson <vitaflo@gmail.com>
2014-03-30 20:33:12 -0500

    removed unneeded JS with the background change.

--- static/js/coal.js
+++ static/js/coal.js
@@ -4,19 +4,6 @@
 // Based on jQuery
 // ==========================================================================
 
-var bg, bgh, bgw;
-
-function resize_bg() {
-    bgw = bg.width();
-    offsetw = bgw - $(window).width();
-    bg.css("left", offsetw/2*-1);
-}
-
 $(function() {
-    bg = $("#bg");
-    resize_bg();
 
-    $(window).resize(function () {
-        resize_bg();
-    });
 });
